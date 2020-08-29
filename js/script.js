fab40bc8c3f101331c35a22a27fa7533191d4bcb
Matthew Gunn <mggunn1@gmail.com>
2020-08-28 18:25:10 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 // javascript dark mode implementation 
 
+
+var darkcounter = 1;
+document.getElementById("darkmode").onclick = function() {darkmode()};
+function darkmode(){
+	darkcounter = darkcounter * (-1);
+	console.log(darkcounter);
+	
+	if(darkcounter < 0){
+		//darkmode color change
+		console.log("Darkmode");
+		
+		//document.getElementById("demo").style.color = "red";
+		
+		
+		
+		
+	}
+	if(darkcounter > 0){
+		//lightmode color change
+		console.log("lightmode");
+		
+		
+	}
+	
+	
+}
+
+
+//things to target to change:
